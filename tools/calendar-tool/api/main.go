6b3e55e94377a05ecc18e518253f8210f902cfc5
Rafael Julio <rafael.julio.dev@outlook.com>
2026-04-30 20:02:22 -0300

    Feat - Initial calendar tool structure

--- tools/calendar-tool/api/main.go
+++ tools/calendar-tool/api/main.go
@@ -1,5 +1,13 @@
 package api
 
+import (
+	_ "data_numbers/internal/handlers"
+	_ "data_numbers/internal/repository"
+	_ "data_numbers/internal/services"
+)
+
 func Main() {
 
 }
+
+// TODO IMPLEMENT
---
+++ tools/calendar-tool/go.mod
@@ -0,0 +1,3 @@
+module data_numbers
+
+go 1.25.2
